eb5bb9de3bfb8986e258da1ce756d77df57f0abf
Mike <mike@agentic.aero>
2025-09-28 03:49:51 -0400

    Change heading and subheading in page component
    Updated the main heading and added a subheading.

--- app/page.tsx
+++ app/page.tsx
@@ -33,20 +33,8 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <h1>Lampoon.xyz - Satire is American.</h1>
+        <h2> Under Construction please excuse our mess</h2>
     </main>
   );
 }
